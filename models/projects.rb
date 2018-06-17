5f3a1b5d2acac4c05640d91e00ed054415f7b0e8
jamos90 <jamos_98@hotmail.com>
2018-06-17 14:16:57 +0100

    inner join returning empty array

--- models/projects.rb
+++ models/projects.rb
@@ -19,7 +19,7 @@ class Project
       type,
       location,
       specilsm_required,
-      age_requirment
+      age_requirement
       )VALUES (
         $1,$2,$3,$4,$5
         ) RETURNING *"
@@ -46,12 +46,22 @@ class Project
       type,
       location,
       specilsm_required,
-      age_requirment
+      age_requirement
     ) =
     ($1,$2,$3,$4,$5)
     WHERE id = $6"
-    values = [@name, @type, @location, @specilsm_required,@age_requirement]
+    values = [@name, @type, @location, @specilsm_required,@age_requirement, @id]
     SqlRunner.run(sql,values)
+  end
+
+  def volunteers()
+    sql = "SELECT volunteers.* FROM volunteers
+    INNER JOIN bookings
+    ON bookings.volunteer_id = volunteers.id
+    WHERE projects_id = $1"
+    values = [@id]
+    results = SqlRunner.run(sql,values)
+    return results.map {|volunteer| Volunteer.new(volunteer)}
   end
 
 end
